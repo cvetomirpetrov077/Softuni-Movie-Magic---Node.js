f840f8820f8624a9472a6433eba1b5cc76c5b3c0
cvetomirpetrov077 <cvetomirpetrov077@gmail.com>
2025-05-31 03:50:05 +0300

    visualized create page and working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import handlebars from "express-handlebars";
 import homeController from "./controllers/homeController.js";
+import movieController from "./controllers/movieController.js";
 
 // init express instance
 const app = express();
@@ -24,6 +25,7 @@ app.set('views', './src/views')
 // Config routes
 
 app.use(homeController);
+app.use('/movies', movieController);
 
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
