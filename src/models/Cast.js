9251691cc2812f8b3e88428d88eeed74c6bf4c99
cvetomirpetrov077 <cvetomirpetrov077@gmail.com>
2025-06-10 00:48:02 +0300

    Added a cast DB with an actor from cast

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,4 +1,4 @@
-import { Schema, model, model } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const castSchema =  new Schema({
 
---
+++ src/services/castService.js
@@ -0,0 +1,8 @@
+import { create } from "domain";
+import Cast from "../models/Cast.js";
+
+export default{
+    create(castData){
+      return Cast.create(castData);
+    }
+}
