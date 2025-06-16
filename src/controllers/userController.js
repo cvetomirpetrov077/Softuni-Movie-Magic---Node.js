36dfe99794f9d29424bc136d545f3fde7c97c3f9
cvetomirpetrov077 <cvetomirpetrov077@gmail.com>
2025-06-17 00:48:56 +0300

    Add user model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
+import { log } from "console";
 import { Router } from "express";
+import userService from "../services/userService.js";
 
 const userController =  Router();
 
@@ -7,6 +9,20 @@ const userController =  Router();
 userController.get('/register', (req, res) => {
     
     res.render('user/register');
+})
+
+userController.post('/register', async (req, res) => {
+
+    // Get data form request
+    // const { email, password, rePassword }= req.body;
+    const userData = req.body;
+    
+    // Register user 
+    await userService.register(userData);
+
+    // Redirect to login 
+
+    res.redirect('/users/login');
 })
 
 
---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+import { type } from "os";
+
+const userSchema =  new Shema({
+
+    email: {
+        type: String,
+        required: true,
+    },
+    password : {
+        type: String, 
+        required: true,
+    }
+})
+
+const User = model('User', userSchema);
+
+export default User ;
